045bdf63bd4f52e1a20328d54bccfa4717b367fe
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-04-03 13:18:50 +0300

    implement tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Init project!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Init project!
+      </h1>
+    </div>
   )
 }
 
