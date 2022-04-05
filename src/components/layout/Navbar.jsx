da44c0d93f2ea51c448880de3bc90c6a2d4ada0a
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-04-05 21:25:51 +0300

    implement react router and work woth navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,29 @@
-function Navbar() {
+import { FaGithub } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+function Navbar({ title }) {
+    console.log('title :>> ', title);
     return (
-        <div>Navbar</div>
+        <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
+            <div className="container mx-auto">
+                <div className="flex-none px-2 mx-2">
+                    <FaGithub className='inline pr-2 text-3xl' />
+                    <Link to={'/'} className='text-lg font-bold align-middle'>
+                        {title}
+                    </Link>
+                </div>
+            </div>
+        </nav>
     )
+}
+
+Navbar.defaultProps = {
+    title: 'GitHubFinder'
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string,
 }
 
 export default Navbar
