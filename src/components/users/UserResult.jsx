9cab92ef168b3086ec125f0e5e12ce310175c2bf
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-05-04 22:31:19 +0300

    make request for all users in userrelut

---
+++ src/components/users/UserResult.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+
+function UserResult() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    const fetchUsers = async () => {
+        const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+            headers: {
+                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+            }
+        })
+
+        const answer = await res.json();
+        setUsers(answer);
+        setLoading(false);
+    }
+
+    if (loading) return <h3>loading</h3>
+
+    return (
+        <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+            {users.map(user => {
+                return <h1>{user.login}</h1>
+            })}
+        </div>
+    )
+}
+
+export default UserResult
