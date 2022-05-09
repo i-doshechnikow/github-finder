a1a8bc3ca8cce6514e7443cdcbfe7ba9954b490b
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-05-09 13:22:05 +0300

    asd

--- src/components/users/UserResult.jsx
+++ src/components/users/UserResult.jsx
@@ -1,26 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useContext } from "react";
+import GithubContext from "../context/github/GithubContext";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
 
 function UserResult() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
   }, []);
-
-  const fetchUsers = async () => {
-    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-
-    const answer = await res.json();
-    setUsers(answer);
-    setLoading(false);
-  };
 
   if (loading) return <Spinner />;
 
