64dad81c080287571ad9f437aad1c8d6bb771c16
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-05-20 22:29:07 +0300

    create info-block for user repos

--- src/components/repos/ReposList.jsx
+++ src/components/repos/ReposList.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
+import RepoItem from "./RepoItem";
 
 export default ({ repos }) => {
-    return <div className='rounded-lg shadow-lg card bg-base-100'>
-        <div className="card-body">
-            <h2 className='text-3xl my-4 font-bold card-title'>
-                Repositories
-            </h2>
-            {repos.map(repo => (<span key={repo.id}>{repo.name}</span>))}
-        </div>
+  return (
+    <div className="rounded-lg shadow-lg card bg-base-100">
+      <div className="card-body">
+        <h2 className="text-3xl my-4 font-bold card-title">Repositories</h2>
+        {repos.map((repo) => (
+          <RepoItem key={repo.id} repo={repo} />
+        ))}
+      </div>
     </div>
-}
+  );
+};
